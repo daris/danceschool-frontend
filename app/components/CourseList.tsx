1c8519239176d98f22a97834041d9f987ee122b4
daris <daris91@gmail.com>
2025-11-15 17:07:36 +0100

    rewrite course list to mui list instead of table

--- app/components/CourseList.tsx
+++ app/components/CourseList.tsx
@@ -4,13 +4,7 @@ import Link from "next/link";
 import {useAppDispatch, useAppSelector} from "@/lib/hooks";
 import {loadCourses, selectCourses, selectStatus} from "@/lib/features/courses/coursesApiSlice";
 import {loadUsers} from "@/lib/features/users/usersApiSlice";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import {Box, LinearProgress} from "@mui/material";
+import {Box, LinearProgress, List, ListItem, ListItemButton} from "@mui/material";
 
 export const CourseList = () => {
   const dispatch = useAppDispatch();
@@ -39,22 +33,16 @@ export const CourseList = () => {
           <LinearProgress />
         </Box>
       }
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableBody>
-            {courses && courses.map((course) => (
-              <TableRow
-                key={course.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <Link key={course.id} href={"/courses/" + course.id}>{course.name} {course.level}</Link>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+
+      <List>
+        {courses && courses.map((course) => (
+          <ListItem key={course.id} disablePadding>
+            <ListItemButton component={Link} href={"/courses/" + course.id}>
+              {course.name} {course.level}
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 };
